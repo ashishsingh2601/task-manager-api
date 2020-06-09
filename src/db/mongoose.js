1df54cc93469f69d7fdbd1345e148df56ba9fa1e
Ashish <as7220004@gmail.com>
2020-06-09 21:34:12 +0530

    Model updated- validator added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -12,20 +13,74 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
         name: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value){
+                if(!validator.isEmail(value)){
+                    throw new Error('Invalid E-mail');
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 7,
+            validate(value){
+                if(value.toLowerCase().includes('password'))
+                throw new Error("Password cannot contain 'password' ");
+            }
         },
         age: {
-            type: Number
+            type: Number,
+            default: 0,
+            validate(value){
+                if(value<0){
+                    throw new Error('Age must be a positive number');
+                }
+            }
         }
 });
 
-const me = new User({
-    name: 'Ashish',
-    age: 21
+// const me = new User({
+//     name: 'Ashish',
+//     email: 'as7220004@gmail.com',
+//     age: 21,
+//     password: 'hello123'
+// });
+
+// me.save().then((me)=>{
+//     console.log(me);
+// }).catch((error)=>{
+//     console.log(error);
+// });
+
+const Tasks = mongoose.model('Tasks', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
-me.save().then((me)=>{
-    console.log(me);
+const task = new Tasks({
+    description: 'Plant Trees',
+    completed: true
+});
+
+task.save().then((task)=>{
+    console.log(task);
 }).catch((error)=>{
     console.log(error);
 });
