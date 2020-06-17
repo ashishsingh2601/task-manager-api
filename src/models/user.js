8c128b94a99012dc6122fb136c67d9f6489536c5
Ashish <as7220004@gmail.com>
2020-06-17 15:28:36 +0530

    Authentication tokens added using JSON web tokens

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -40,8 +41,23 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number');
             }
         }
-    }
+    },
+    tokens: [{
+        token:{
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.methods.generateAuthToken = async function(){
+    const user = this;
+    const token = jwt.sign({_id: user._id.toString}, 'learningnode');
+
+    user.tokens = user.tokens.concat({token: token});
+    await user.save();
+    return token;    
+};
 
 userSchema.statics.findByCredentials = async (email, password)=>{
     const user = await User.findOne({ email: email});
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,13 @@ router.post('/users', async (req, res)=>{
     const user = new User(req.body);
     try{
         await user.save();
-        res.status(201).send(user);
+       // const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        
+        res.status(201).send({
+            user: user,
+            token: token
+        });
     } catch(e){
         res.status(400).send(e);
     }
@@ -28,7 +34,11 @@ router.post('/users/login', async (req, res)=>{
     try{
         
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({
+            user: user, 
+            token: token
+        });
     }catch(e){
         res.status(400).send(e);
     }
