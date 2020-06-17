b1a9508f7e47fac840a5e9ffa134c71b84bd469e
Ashish <as7220004@gmail.com>
2020-06-17 14:02:00 +0530

    Login route and model added

--- src/routers/user.js
+++ src/routers/user.js
@@ -23,6 +23,17 @@ router.post('/users', async (req, res)=>{
     // });
 });
 
+//Login route
+router.post('/users/login', async (req, res)=>{
+    try{
+        
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        res.send(user);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+
 //Reading multiple users
 router.get('/users', async (req, res)=>{
    
