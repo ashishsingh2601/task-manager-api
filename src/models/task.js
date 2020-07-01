a5b79e84f793ee9d7d01c40507cf01732affaa74
Ashish <as7220004@gmail.com>
2020-07-01 16:44:55 +0530

    Timestamps added.

--- src/models/task.js
+++ src/models/task.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 //const validator = require('validator');
 
-const Tasks = mongoose.model('Tasks', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         trim: true,
@@ -18,7 +18,11 @@ const Tasks = mongoose.model('Tasks', {
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 });
+
+const Tasks = mongoose.model('Tasks', taskSchema);
 
 
 
