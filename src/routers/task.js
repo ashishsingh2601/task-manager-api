3630da860e0f54574c8f38ba414ae95b5b73cee0
Ashish <as7220004@gmail.com>
2020-07-02 10:39:10 +0530

    Data pagination and sorting feature added.

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,18 +28,30 @@ router.post('/tasks', auth, async (req, res)=>{
 
 //Reading multiple tasks
 //GET /tasks?completed=true
+//GET /tasks?limit=10&skip=0
+//GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res)=>{
     const match = {};
+    const sort = {};
         
     if(req.query.completed){
             match.completed = req.query.completed === 'true';
         }
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split('_');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
     
     try{
     //const tasks = await Tasks.find({ owner: req.user._id });
     await req.user.populate({
         path: 'tasks',
-        match
+        match,
+        options: {
+            limit: parseInt(req.query.limit, 10),
+            skip: parseInt(req.query.skip, 10),
+            sort
+        }
     }).execPopulate();
     res.status(200).send(req.user.tasks);
    }catch(e){
