47c02ddbb62b32a3c4ce26e093fc369047ff4808
Ashish <as7220004@gmail.com>
2020-07-04 00:46:21 +0530

    Auto-cropping and image formatting enabled.

--- src/routers/user.js
+++ src/routers/user.js
@@ -169,7 +169,8 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=>{
-    req.user.avatar =  req.file.buffer;
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
@@ -189,7 +190,7 @@ router.get('/users/:id/avatar', async(req, res)=>{
             throw new Error();
         }
 
-        res.set('Content-Type', 'image/jpg');
+        res.set('Content-Type', 'image/png');
         res.send(user.avatar);
     }catch(e){
         res.status(404).send();
